dce0222a73488694b13e302d50b8a4d5b01a24b9
jayd7 <jayd7@vt.edu>
2017-05-04 17:56:47 -0400

    added functionality to return indices of binned forces

--- getconstVCreep.m
+++ getconstVCreep.m
@@ -1,4 +1,4 @@
-function [constVCreep, constVfx,constVfz] = getconstVCreep(fz,fx,binC,binWidth)
+function [constVCreep, constVfx,constVfz,Kf] = getconstVCreep(fz,fx,binC,binWidth)
 Kf = find(((fz <= binC + binWidth/2) & (fz >= binC - binWidth/2)));
 constVfx = fx(Kf);
 constVfz = fz(Kf);
